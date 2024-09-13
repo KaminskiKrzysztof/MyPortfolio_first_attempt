99010000cbbfe292a32891315fbb82eb8d300297
krzysztof <krzysztofkaminski022@gmail.com>
2024-09-13 02:10:30 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
+window.onscroll = function () {
+	let scrollToTopButton = document.getElementById('scrollToTop');
+	if (
+		document.body.scrollTop > 350 ||
+		document.documentElement.scrollTop > 350
+	) {
+		scrollToTopButton.style.display = 'block';
+	} else {
+		scrollToTopButton.style.display = 'none';
+	}
+};
 
+document.getElementById('scrollToTop').addEventListener('click', function () {
+	window.scrollTo({ top: 0, behavior: 'smooth' });
+});
