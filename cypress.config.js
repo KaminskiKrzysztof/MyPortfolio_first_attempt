3e12005fade01037236970ca178e3dae34b22445
krzysztof <krzysztofkaminski022@gmail.com>
2024-10-15 19:17:18 +0200

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	viewportHeight: 900,
+	viewportWidth: 1600,
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
+	defaultCommandTimeout: 15000,
 });
