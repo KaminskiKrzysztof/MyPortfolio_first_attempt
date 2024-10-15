748ae94b95394d427bf717ded7dda79622033458
krzysztof <krzysztofkaminski022@gmail.com>
2024-10-15 19:22:36 +0200

    update

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,9 @@
 describe('testing', () => {
 	it('open myportoflio homepage', () => {
-		cy.visit('https://kaminskikrzysztof.github.io/My-Portoflio/');
+		cy.visit('http://localhost:3000/');
 		cy.get('.text-name').should('contain', 'Krzysztof Kamiński'),
 			cy.get('.nav-link').should('contain', 'Contact');
 		cy.get('footer').scrollIntoView();
-		cy.get('.input-one');
+		cy.get('.input-one').click('.input-one').should('contain', '');
 	});
 });
