f36c763d08106c70b80704172ea61b9872f205de
krzysztof <krzysztofkaminski022@gmail.com>
2024-10-17 20:01:32 +0200

    update

---
+++ cypress/e2e/open_links.cy.js
@@ -0,0 +1,13 @@
+describe('open_links', () => {
+	it('open different links', () => {
+		cy.visit('http://localhost:3000/');
+		cy.get('.text-name');
+		cy.get('.icon-box');
+		cy.get('.github-link').click();
+		cy.wait(2000);
+		cy.get('.linkedin-link').click();
+		cy.get('.footer').scrollIntoView();
+		cy.wait(1000);
+		cy.get('.go-up > .go-up-arrow').click();
+	});
+});
